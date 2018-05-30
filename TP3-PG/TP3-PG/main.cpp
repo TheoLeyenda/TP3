e9be10efd79b8fe17b5426e2298f3e28c231baf0
Unknown <theoleyenda1997@gmail.com>
2018-05-30 12:42:26 -0300

    funcion de dibujar

--- TP3-PG/TP3-PG/main.cpp
+++ TP3-PG/TP3-PG/main.cpp
@@ -4,16 +4,25 @@
 const float FPS = 60;
 const int SCREEN_W = 640;
 const int SCREEN_H = 480;
-const int BOUNCER_SIZE = 32;
+const int JUGADOR_SIZE = 32;
 
+void draw(ALLEGRO_BITMAP *_bitmap,float X_bitmap, float Y_bitmap, int flags)
+{
+	al_draw_bitmap(_bitmap, X_bitmap, Y_bitmap, flags);
+}
+void movimientoJugador(float X_jugador, float Y_jugador)
+{
+
+}
 int main(int argc, char **argv)
 {
+	bool gameOver = false;
 	ALLEGRO_DISPLAY *display = NULL;
 	ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 	ALLEGRO_TIMER *timer = NULL;
-	ALLEGRO_BITMAP *bouncer = NULL;
-	float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
-	float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+	ALLEGRO_BITMAP *Jugador = NULL;
+	float Jugador_x = SCREEN_W / 2.0 - JUGADOR_SIZE / 2.0;
+	float Jugador_y = SCREEN_H / 2.0 - JUGADOR_SIZE / 2.0;
 	bool redraw = true;
 
 	if (!al_init()) {
@@ -39,15 +48,15 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
-	bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
-	if (!bouncer) {
+	Jugador = al_create_bitmap(JUGADOR_SIZE, JUGADOR_SIZE);
+	if (!Jugador) {
 		fprintf(stderr, "failed to create bouncer bitmap!\n");
 		al_destroy_display(display);
 		al_destroy_timer(timer);
 		return -1;
 	}
 
-	al_set_target_bitmap(bouncer);
+	al_set_target_bitmap(Jugador);
 
 	al_clear_to_color(al_map_rgb(255, 0, 255));
 
@@ -56,7 +65,7 @@ int main(int argc, char **argv)
 	event_queue = al_create_event_queue();
 	if (!event_queue) {
 		fprintf(stderr, "failed to create event_queue!\n");
-		al_destroy_bitmap(bouncer);
+		al_destroy_bitmap(Jugador);
 		al_destroy_display(display);
 		al_destroy_timer(timer);
 		return -1;
@@ -74,7 +83,7 @@ int main(int argc, char **argv)
 
 	al_start_timer(timer);
 
-	while (1)
+	while (!gameOver)
 	{
 		ALLEGRO_EVENT ev;
 		al_wait_for_event(event_queue, &ev);
@@ -85,28 +94,19 @@ int main(int argc, char **argv)
 		else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			break;
 		}
-		else if (ev.type == ALLEGRO_EVENT_MOUSE_AXES ||
-			ev.type == ALLEGRO_EVENT_MOUSE_ENTER_DISPLAY) {
-
-			bouncer_x = ev.mouse.x;
-			bouncer_y = ev.mouse.y;
-		}
-		else if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-			break;
-		}
 
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			redraw = false;
 
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 
-			al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+			draw(Jugador, Jugador_x, Jugador_y, 0);
 
 			al_flip_display();
 		}
 	}
 
-	al_destroy_bitmap(bouncer);
+	al_destroy_bitmap(Jugador);
 	al_destroy_timer(timer);
 	al_destroy_display(display);
 	al_destroy_event_queue(event_queue);
